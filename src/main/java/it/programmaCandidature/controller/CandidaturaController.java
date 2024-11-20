a7d169b9d35096052841931c1c84058f58e1b3ae
Ofelia <o.sestu.96@gmail.com>
2024-11-20 12:37:30 +0100

    Update CandidaturaController.java

--- src/main/java/it/programmaCandidature/controller/CandidaturaController.java
+++ src/main/java/it/programmaCandidature/controller/CandidaturaController.java
@@ -15,17 +15,18 @@ import it.programmaCandidature.model.Utenti;
 import it.programmaCandidature.repository.CandidaturaRepository;
 import it.programmaCandidature.repository.UtenteRepository;
 
-@Controller //Definisce la classe come un controller Spring MVC, gestisce richieste HTTP e restituisce viste (file HTML o JSP)
-@RequestMapping ("/candidatura") //I metodi di questa classe risponderanno a richieste il cui percorso inizia con /candidatura
+@Controller 
+@RequestMapping ("/candidatura") 
 public class CandidaturaController {
 	
-	@Autowired //inietta automaticamente una dipendenza nella classe (in questo caso CandidaturaRepository e UtenteRepository)
+	@Autowired 
 	private CandidaturaRepository repo;
 	@Autowired
 	private UtenteRepository utenteRepository;
 	
+	//cerca l'utente e carica la sua pagina di candidature
 	@GetMapping ("/lista")
-	public String lista (Model model, Principal principal) { //metodo che mostra i dati del database tramite la classe Model
+	public String lista (Model model, Principal principal) { 
 		String username = principal.getName();
 		Utenti utente = utenteRepository.findByUsername(username);
 		Iterable<Candidatura> candidatura = repo.findByUtente(utente);
@@ -37,27 +38,4 @@ public class CandidaturaController {
 		model.addAttribute ("candidature", candidatura);
 		return "candidature";
 	}
-	
-	/*@GetMapping ("/lista") //metodo che risponde alle richieste GET all'URL /candidatura/lista
-	public String lista() {
-		Iterable<Candidatura> candidatura = repo.findAll(); //Usa il respository (repo) per recuperare tutte le entita` Candidatura dal database
-		
-		candidatura.forEach((Candidatura c) -> {
-			System.out.println(c.getId());
-		}); 
-		//Stampa a console l'id di ogni candidatura preso dalla lista 
-		
-		return "candidature"; //Restituisce candidature.ftl
-	}
-	
-	/*@ResponseBody
-	@GetMapping("/test")
-	public String test() {
-		return "Test sul controller.";
-	}
-	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}*/
 }
